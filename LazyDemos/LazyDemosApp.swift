3507d667b1ec88b6e8abd19d5e4b493b2c017051
MacModeler <memcbride@mac.com>
2024-06-25 20:52:53 -0400

    Initial Commit

---
+++ LazyDemos/LazyDemosApp.swift
@@ -0,0 +1,17 @@
+//
+//  LazyDemosApp.swift
+//  LazyDemos
+//
+//  Created by Mark McBride on 6/25/24.
+//
+
+import SwiftUI
+
+@main
+struct LazyDemosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
