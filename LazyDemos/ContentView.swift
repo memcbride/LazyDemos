add7e53d406d5c8bbd56d1cebd744a6af8a46b89
MacModeler <memcbride@mac.com>
2024-06-25 21:42:56 -0400

    After working through the demos in Chapter 4.4

--- LazyDemos/ContentView.swift
+++ LazyDemos/ContentView.swift
@@ -8,17 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var columns = [
+        GridItem(.flexible(maximum: 110)),
+        GridItem(.flexible(maximum: 200))
+    ]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ScrollView {
+            
+            LazyVGrid(columns: columns) {
+                ForEach(0..<1000) { i in
+                    let _ = print("Dang \(i)")
+                    RedAndCyanView()
+                }
+                
+            }
         }
         .padding()
+    }
+}
+
+struct RedAndCyanView: View {
+    var body: some View {
+        ZStack {
+            Color(.red)
+            Rectangle()
+                .fill(.cyan)
+                .frame(width: 100, height: 100)
+        }
     }
 }
 
 #Preview {
     ContentView()
 }
+
